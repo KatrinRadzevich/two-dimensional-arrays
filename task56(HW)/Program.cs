63929be8735e30b3da0cff81f2cfcae6d70c94a6
Karina Radzevich <karina_radzevich@bk.ru>
2023-03-10 23:53:31 +0400

    HW fro seminar 8 is done

---
+++ task56(HW)/Program.cs
@@ -0,0 +1,103 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+// наименьшей суммой элементов: 1 строка
+
+// !Все закоментированные части существуют для легкой проверки/самопроверки
+// и наглядности работы кода. в этом виде в консоли вывод такой же как в примере!
+
+int[,] array = new int[4, 4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int[] rowArray = GetRowArray(array);
+// Console.WriteLine(String.Join(" ", rowArray));
+// Console.WriteLine();
+
+int[] firstPartOfRowArray = GetLeftPartOfArray(rowArray);
+int[] secondPartOfRowArray = GetRightPartOfArray(rowArray);
+int[] firstString = GetLeftPartOfArray(firstPartOfRowArray);
+int[] secondString = GetRightPartOfArray(firstPartOfRowArray);
+int[] thirdString = GetLeftPartOfArray(secondPartOfRowArray);
+int[] fourthString = GetRightPartOfArray(secondPartOfRowArray);
+
+int sum0 = GetSumRow(firstString);
+int sum1 = GetSumRow(secondString);
+int sum2 = GetSumRow(thirdString);
+int sum3 = GetSumRow(fourthString);
+GetResult(sum0, sum1, sum2, sum3);
+// Console.WriteLine($"{String.Join(", ", firstString)}  Сумма строки = {sum0}");
+// Console.WriteLine($"{String.Join(", ", secondString)} Сумма строки = {sum1}");
+// Console.WriteLine($"{String.Join(", ", thirdString)} Сумма строки = {sum2}");
+// Console.WriteLine($"{String.Join(", ", fourthString)} Сумма строки = {sum3}");
+
+void FillArray(int[,] someArray)
+{
+    for (int i = 0; i < someArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < someArray.GetLength(1); j++)
+        {
+            someArray[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] anyArray)
+{
+    for (int k = 0; k < anyArray.GetLength(0); k++)
+    {
+        for (int l = 0; l < anyArray.GetLength(1); l++)
+        {
+            Console.Write($"{anyArray[k, l]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0) * inArray.GetLength(1)];
+    int index = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[index] = inArray[i, j];
+            index++;
+        }
+    }
+    return result;
+}
+int[] GetLeftPartOfArray(int[] inArray)
+{
+    int mid = (inArray.Length + 1) / 2;
+    int[] someArray = inArray[..mid];
+    return someArray;
+}
+int[] GetRightPartOfArray(int[] inArray)
+{
+    int mid = (inArray.Length + 1) / 2;
+    int[] someArray = inArray[mid..];
+    return someArray;
+}
+int GetSumRow(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+void GetResult(int sum, int sum1, int sum2, int sum3)
+{
+    int result = 1;
+    if (sum1 < sum) result = 2;
+    if (sum2 < sum) result = 3;
+    if (sum3 < sum) result = 4;
+    Console.WriteLine($"Наименьшая строка {result}");
+}
---
+++ task58(HW)/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение
+// двух матриц.Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int size = 2;
+int[,] array1 = FillArray(size, size, 1, 10);
+int[,] array2 = FillArray(size, size, 1, 10);
+int[,] arrayResult = GetMultiplicationArray(array1, array2);
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+Console.WriteLine();
+PrintArray(arrayResult);
+int[,] FillArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] anyArray)
+{
+    for (int k = 0; k < anyArray.GetLength(0); k++)
+    {
+        for (int l = 0; l < anyArray.GetLength(1); l++)
+        {
+            Console.Write($"{anyArray[k, l]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMultiplicationArray(int[,] inArray, int[,] inArray2)
+{
+    int[,] array = new int[inArray.GetLength(0), inArray.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = inArray[i, 0] * inArray2[0, j] + inArray[i, 1] * inArray2[1, j];
+        }
+    }
+    return array;
+}
