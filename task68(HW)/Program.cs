e0d23360c077900ff6e12f71df501168a63f6238
Karina Radzevich <karina_radzevich@bk.ru>
2023-03-14 03:30:04 +0400

    final project C# is done

---
+++ task68(HW)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Write("Чтобы увидеть сумму натуральных элементов в промежутке от M до N, введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Также ведите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {A(m, n)}");
+int A(int k, int l)
+{
+    if (k == 0)
+        return l + 1;
+    else
+      if ((k != 0) && (l == 0))
+        return A(k - 1, 1);
+    else
+        return A(k - 1, A(k, l - 1));
+}
